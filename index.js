b3c9909cf75a92ec2b6fd3c0aaa1fae6554f9426
Raehale <alyssarae98ha@gmail.com>
2024-10-07 11:00:11 -0600

    made function, WIP

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("authorEl").textContent = `By: ${data.user.name}`
+        getColors(data);
     })
     .catch(err => {
         // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("authorEl").textContent = `By: Dodi Achmad`
-    })
-
-    
-
-    // fetch('https://get-quotes-api.p.rapidapi.com/id/%7Bid%7D/ab40763e11mshc74fd069866938bp138b87jsn68af667c9eca')
-    // .then(res => res.json())
-    // .then(data => console.log(data))
-	
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`;
+		document.getElementById("authorEl").textContent = `By: Dodi Achmad`;
+    });
 
     const url = 'https://get-quotes-api.p.rapidapi.com/random';
     const options = {
@@ -30,21 +23,45 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-       
-         document.getElementById("root").textContent = `${ result.quote.quote}`
-    
-        document.getElementById("author").textContent = `${result.quote.author}`
 
+        document.getElementById("root").textContent = `${ result.quote.quote}`
+        document.getElementById("author").textContent = `${result.quote.author}`
     } catch (error) {
         console.error(error);
     }
 
-    // result = {
-    //         "message":"Quote of the Day",
-    //         "quote": {
-    //             "id":111,
-    //             "category":"Motivation",
-    //             "quote":"The only way to do great work is to love what you do.",
-    //             "author":"Steve Jobs",
-    //             "description":"An inspiring quote emphasizing the importance of passion in one's work."}
+//get color from background
+function getColors(data) {
+    // const imageFile = data;
+    // const image = new Image();
+    // const file = imageFile;
+    // const fileReader = new FileReader();
+
+    // fileReader.onload = () => {
+    //     image.onload = () => {
+    //         const canvas = document.getElementById("canvas");
+    //         canvas.width = image.width;
+    //         canvas.height = image.height;
+    //         const ctx = canvas.getContext("2d");
+    //         console.log(ctx)
+
+    //         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    //         console.log(imageData)
+    //         console.log('hi')
     //     }
+    // }
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const image = new Image();
+    image.src = data.urls.regular;
+    image.addEventListener("load", () => {
+        ctx.drawImage(image, 0, 0, 233, 320);
+
+        const imageData = ctx.getImageData(10, 20, 80, 230);
+        ctx.putImageData(imageData, 260, 0);
+        ctx.putImageData(imageData, 380, 50);
+        ctx.putImageData(imageData, 500, 100);
+    })
+}
