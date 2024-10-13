99b8fe2df1ec4381d1dd23eba2b3890c7b09bd2a
Raehale <alyssarae98ha@gmail.com>
2024-10-12 21:21:25 -0600

    added color grabbing API and changed colors

--- index.js
+++ index.js
@@ -1,67 +1,65 @@
+import ColorThief from './node_modules/colorthief/dist/color-thief.mjs'
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("authorEl").textContent = `By: ${data.user.name}`
-        getColors(data);
+        getColor(data.urls.regular)
     })
     .catch(err => {
         // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`;
-		document.getElementById("authorEl").textContent = `By: Dodi Achmad`;
-    });
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
+		document.getElementById("authorEl").textContent = `By: Dodi Achmad`
 
-    const url = 'https://get-quotes-api.p.rapidapi.com/random';
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': 'ab40763e11mshc74fd069866938bp138b87jsn68af667c9eca',
-            'x-rapidapi-host': 'get-quotes-api.p.rapidapi.com'
-        }
-    };
-    
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-
-        document.getElementById("root").textContent = `${ result.quote.quote}`
-        document.getElementById("author").textContent = `${result.quote.author}`
-    } catch (error) {
-        console.error(error);
-    }
-
-//get color from background
-function getColors(data) {
-    // const imageFile = data;
-    // const image = new Image();
-    // const file = imageFile;
-    // const fileReader = new FileReader();
-
-    // fileReader.onload = () => {
-    //     image.onload = () => {
-    //         const canvas = document.getElementById("canvas");
-    //         canvas.width = image.width;
-    //         canvas.height = image.height;
-    //         const ctx = canvas.getContext("2d");
-    //         console.log(ctx)
-
-    //         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    //         console.log(imageData)
-    //         console.log('hi')
-    //     }
-    // }
-
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-
-    const image = new Image();
-    image.src = data.urls.regular;
-    image.addEventListener("load", () => {
-        ctx.drawImage(image, 0, 0, 233, 320);
-
-        const imageData = ctx.getImageData(10, 20, 80, 230);
-        ctx.putImageData(imageData, 260, 0);
-        ctx.putImageData(imageData, 380, 50);
-        ctx.putImageData(imageData, 500, 100);
+        console.error(err)
     })
+
+    const url = 'https://get-quotes-api.p.rapidapi.com/random'
+    const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'ab40763e11mshc74fd069866938bp138b87jsn68af667c9eca',
+        'x-rapidapi-host': 'get-quotes-api.p.rapidapi.com'
+    }
+};
+
+try {
+    const response = await fetch(url, options)
+    const result = await response.json()
+
+    document.getElementById("root").textContent = `${ result.quote.quote}`
+    document.getElementById("author").textContent = `${result.quote.author}`
+} catch (error) {
+    console.error(error)
+}
+
+//get colors from background
+function getColor(backgroundImage) {
+    try {
+        const colorThief = new ColorThief()
+        const img = new Image()
+
+        img.crossOrigin = 'Anonymous'
+        img.src = `${backgroundImage}`
+
+        img.addEventListener('load', function() {
+            changeTextBgColor(colorThief.getPalette(img))
+        })
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+//change text background colors to new colors
+function changeTextBgColor(rgbPalette) {
+    let backgroundColor = `rgb(${rgbPalette[0][0]}, ${rgbPalette[0][1]}, ${rgbPalette[0][2]})`
+    let textColor = `rgb(${rgbPalette[1][0]}, ${rgbPalette[1][1]}, ${rgbPalette[1][2]})`
+
+    Array.from(document.getElementsByClassName("background-color-change")).forEach((el) => {
+        el.style.backgroundColor = backgroundColor
+        el.style.color = textColor
+    })
+
+    document.getElementById("submitBtn").style.color = textColor
 }
