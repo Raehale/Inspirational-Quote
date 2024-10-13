cf3251439467d6d3592396540e86e0f51646b801
Raehale <alyssarae98ha@gmail.com>
2024-10-13 10:16:17 -0600

    added change theme funcitonality and stored theme to local storage

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("authorEl").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("authorEl").textContent = `By: Dodi Achmad`
-    })
-
+if (localStorage.getItem('theme')) {
+    backgroundGenerator(localStorage.getItem('theme'))
+} else {
+    backgroundGenerator("nature")
+}
+function backgroundGenerator(backgroundTheme) {
+    fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${backgroundTheme}`)
+        .then(res => res.json())
+        .then(data => {
+            document.body.style.backgroundImage = `url(${data.urls.regular})`
+            document.getElementById("authorEl").textContent = `By: ${data.user.name}`
+        })
+        .catch(err => {
+            // Use a default background image/author
+            document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+    )`
+            document.getElementById("authorEl").textContent = `By: Dodi Achmad`
+        })
+}
     
 
     // fetch('https://get-quotes-api.p.rapidapi.com/id/%7Bid%7D/ab40763e11mshc74fd069866938bp138b87jsn68af667c9eca')
@@ -39,12 +45,11 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         console.error(error);
     }
 
-    // result = {
-    //         "message":"Quote of the Day",
-    //         "quote": {
-    //             "id":111,
-    //             "category":"Motivation",
-    //             "quote":"The only way to do great work is to love what you do.",
-    //             "author":"Steve Jobs",
-    //             "description":"An inspiring quote emphasizing the importance of passion in one's work."}
-    //     }
+document.getElementById("themeInputBtn").addEventListener("click", () => {
+    changeTheme(document.getElementById("themeInput").value)
+})
+
+function changeTheme(newTheme) {
+    backgroundGenerator(newTheme)
+    localStorage.setItem('theme', newTheme)
+}
